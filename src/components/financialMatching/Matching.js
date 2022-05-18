363d6e760a95544767072c6a331bbcdbff165679
권설아 <sulah0326@naver.com>
2022-05-19 01:44:32 +0900

    Feat: 금융매칭 버튼 div 추가

--- src/components/financialMatching/Matching.js
+++ src/components/financialMatching/Matching.js
@@ -12,18 +12,22 @@ const Matching = () => {
         <div>
             <Grid container style={{marginTop:"80px"}}>
                 <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center'}}>
-                    <Link to='/cards/questions'>
-                        <button className={clsx('btn_1', 'margin_40')}>
-                            나의 BEST 카드 찾기
-                        </button>
-                    </Link>
+                    <div className='margin_40'>
+                        <Link to='/cards/questions'>
+                            <button className={clsx('btn_1')}>
+                                나의 BEST 카드 찾기
+                            </button>
+                        </Link>
+                    </div>
                 </Grid>
                 <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center'}}>
-                    <Link to='/cards/ranking'>
-                        <button className={clsx('btn_2', 'margin_40')}>
+                    <div className='margin_40'>
+                        <Link to='/cards/ranking'>
+                            <button className={clsx('btn_2')}>
                             인기카드 Top 10
-                        </button>
-                    </Link>
+                            </button>
+                        </Link>
+                    </div>
                 </Grid>
             </Grid>
         </div>
